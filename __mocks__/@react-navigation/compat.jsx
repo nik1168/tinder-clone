cfb83803dcdb729d3fb58eb60f3fd362ace44ccb
nik1168 <ngeisser32@gmail.com>
2021-12-17 23:44:18 -0400

    feat: fix lint

--- __mocks__/@react-navigation/compat.jsx
+++ __mocks__/@react-navigation/compat.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+// eslint-disable-next-line import/extensions
 import fakeNavigation from '../../src/navigation/__mocks__/navigation';
 
 module.exports = {
-  withNavigation: (Component) => (props) =>
+  withNavigation: Component => props =>
     <Component navigation={fakeNavigation} {...props} />,
-  SafeAreaView: ({ children }) => <>{children}</>,
+  SafeAreaView: ({children}) => <>{children}</>,
 };
--- scr/navigation/StackNavigator.tsx
+++ scr/navigation/StackNavigator.tsx
@@ -1,22 +1,21 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {NavigationContainer} from "@react-navigation/native";
-import HomeScreen from "../views/home-screen";
-import ChatScreen from "../views/chat-screen";
 
+import ChatScreen from '../views/chat-screen';
+import HomeScreen from '../views/home-screen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Chat" component={ChatScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default StackNavigator;
