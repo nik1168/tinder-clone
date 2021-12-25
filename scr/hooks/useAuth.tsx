8b65ebfcbeb0e6164fd8e50d59e7b0b44913e862
nik1168 <ngeisser32@gmail.com>
2021-12-24 23:04:00 -0400

    feat: add logic to authenticate to firebase and get user

--- scr/hooks/useAuth.tsx
+++ scr/hooks/useAuth.tsx
@@ -1,57 +1,72 @@
-import auth from '@react-native-firebase/auth';
-import {
-  GoogleSignin,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
-import React, {createContext, FC, useContext, useEffect} from 'react';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import React, {
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import Config from 'react-native-config';
 
 export interface AuthContextShape {
-  user?: string | null;
+  user?: FirebaseAuthTypes.User | null;
   signInWithGoogle?: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextShape>({});
 
 export const AuthProvider: FC = ({children}): JSX.Element => {
+  const [errorAuth, setErrorAuth] = useState<string | null>(null);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const [loadingInitial, setLoadingInitial] = useState<boolean>(true);
+
+  const onAuthStateChanged = useCallback(
+    (userAuth: FirebaseAuthTypes.User | null) => {
+      if (userAuth) {
+        setUser(userAuth);
+      } else {
+        // Not logged in
+        setUser(null);
+      }
+      setLoadingInitial(false);
+    },
+    [],
+  );
+  const {FIRE_BASE_WEB_CLIENT_ID} = Config;
+
   useEffect(() => {
     GoogleSignin.configure({
-      scopes: ['email', 'profile'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId:
-        '903015749708-mg00ffu80a3bb9jb3b4h4ua5itbptdsh.apps.googleusercontent.com',
+      scopes: ['email', 'profile'],
+      webClientId: FIRE_BASE_WEB_CLIENT_ID,
       offlineAccess: true,
     });
-  }, []);
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+
+    return subscriber; // unsubscribe on unmount
+  }, [FIRE_BASE_WEB_CLIENT_ID, onAuthStateChanged]);
+
   const signInWithGoogle = async () => {
     try {
-      GoogleSignin.configure();
       await GoogleSignin.hasPlayServices();
       const {idToken} = await GoogleSignin.signIn();
       const credential = auth.GoogleAuthProvider.credential(idToken);
 
       await auth().signInWithCredential(credential);
     } catch (error: any) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-        // alert('Cancel');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // alert('Signin in progress');
-        // operation (f.e. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // alert('PLAY_SERVICES_NOT_AVAILABLE');
-        // play services not available or outdated
-      } else {
-        // some other error happened
-      }
+      setErrorAuth(error);
     }
   };
 
   return (
     <AuthContext.Provider
       value={{
-        user: null,
+        user,
         signInWithGoogle,
       }}>
-      {children}
+      {!loadingInitial ? children : null}
+      {errorAuth ?? null}
     </AuthContext.Provider>
   );
 };
