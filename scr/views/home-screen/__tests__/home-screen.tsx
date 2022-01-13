027df96da5273d043a3140f48ad6243bc27be270
nik1168 <ngeisser32@gmail.com>
2022-01-12 21:16:38 -0400

    test(homeScreen): Update tests

--- scr/views/home-screen/__tests__/home-screen.tsx
+++ scr/views/home-screen/__tests__/home-screen.tsx
@@ -1,9 +1,7 @@
-import {fireEvent, render} from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import React from 'react';
 
 import MockedNavigator from '../../../navigation/__mocks__';
-import navigation from '../../../navigation/__mocks__/navigation';
-import {RootStackRouteNames} from '../../../navigation/typings';
 import HomeScreen from '../index';
 
 describe('Home Screen', (): void => {
@@ -14,17 +12,6 @@ describe('Home Screen', (): void => {
       </MockedNavigator>,
     );
 
-    expect(component.getByText('I am the home screen')).toBeTruthy();
-  });
-
-  test('it should navigate to chat screen', async (): Promise<void> => {
-    const component = render(
-      <MockedNavigator>
-        <HomeScreen />
-      </MockedNavigator>,
-    );
-
-    fireEvent(component.getByText('go to chat screen'), 'press');
-    expect(navigation.navigate).toHaveBeenCalledWith(RootStackRouteNames.Chat);
+    expect(component).toBeTruthy();
   });
 });
